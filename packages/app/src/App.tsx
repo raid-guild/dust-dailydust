54e8d5b231b83d295b1092bcba5d024a4f690d7a
ECWireless <elliott@coopallc.com>
2025-08-22 08:53:08 -0600

    fix: switch to react analytics import

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { Layout } from "@/components/Layout";
